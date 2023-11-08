e0200e1a5dbaad2706b827824551df82eeceacab
victoramattosc <victorcarbelotti0306@gmail.com>
2023-11-08 09:00:13 -0300

    ???

--- src/main/java/br/com/mvv/aqceacha/resource/VendedorResource.java
+++ src/main/java/br/com/mvv/aqceacha/resource/VendedorResource.java
@@ -7,6 +7,7 @@ import br.com.mvv.aqceacha.repository.projections.VendedorDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class VendedorResource {
         return vendedorRepository.filtrar(vendedorFilter, pageable);
     }
 
+    @CrossOrigin("*")
     @GetMapping("/todos")
     public List<Vendedor> listarTodosVendedor() {return vendedorRepository.findAll();}
 }
